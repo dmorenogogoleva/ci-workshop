193cd83aae83dde56bf18b644b6b923ce2f261c8
iwanttobealight <iwanttobealight@yandex.ru>
2019-06-14 21:08:26 +0300

    upate app.js

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/user', (req, res) => res.send('user'))
+app.get('/user', (req, res) => res.send('ty pidor!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
